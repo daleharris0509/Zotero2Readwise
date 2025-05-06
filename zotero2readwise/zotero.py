680a42251087c450530fd71ac19237a7995c1a0c
Josie Duffy <josie.duffy.rice@gmail.com>
2025-05-06 19:55:00 -0400

    Fix import for ParamNotPassedError in zotero.py

--- zotero2readwise/zotero.py
+++ zotero2readwise/zotero.py
@@ -4,7 +4,7 @@ from os import environ
 from typing import Dict, List, Optional
 
 from pyzotero.zotero import Zotero
-from pyzotero.zotero_errors import ParamNotPassed, UnsupportedParams
+from pyzotero.zotero_errors import ParamNotPassedError, UnsupportedParams
 
 from zotero2readwise import FAILED_ITEMS_DIR
 
